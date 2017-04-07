db3b2bbdf567bc8806d3d654a66e1a4c018fecd1
Pierre <pierre.trzaska@gmail.com>
2017-04-07 14:43:50 +0200

    reveal done

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,4 +1,7 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".services__service"),"50%");
+new RevealOnScroll($(".skills__element"),"50%");
