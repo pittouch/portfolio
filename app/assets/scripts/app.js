8913fe3fdf2607aa3b6ff4accbcc1bb15d8a2891
Pierre <pierre.trzaska@gmail.com>
2017-04-10 15:48:51 +0200

    sticky-header done

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".services__service"),"50%");
+new RevealOnScroll($(".services__service"),"70%");
 new RevealOnScroll($(".skills__element"),"50%");
+var stickyHeader = new StickyHeader();
